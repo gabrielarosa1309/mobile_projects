1686c46053784050991edb6a9ed698ea22019dcd
Gabriela Ramos Mariano Rosa <50579698890@info.local>
2024-01-29 09:57:57 -0300

    :construction: Revisão de hooks
    Projeto criado para revisão do uso de hooks

---
+++ revisaoHooks/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const[count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text>Contador: {count}</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
