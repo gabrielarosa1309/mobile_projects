a11b5c72f0670886bc1de18f503dd71a8b25e86f
Gabriela Ramos Mariano Rosa <50579698890@info.local>
2024-01-29 09:52:24 -0300

    :heavy_check_mark: Revisão de props finalizada
    Projeto finalizado com o estudo de props utilizando o componente Flatlist

--- revisaoProps/App.js
+++ revisaoProps/App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { FlatList, SafeAreaView } from 'react-native';
 import Person from './src/components/Person/Person';
 
 export default function App() {
+  //Simulação de uma lista de pessoas 
+  const peopleList = [
+    {id: '1', name: 'Carlos', age: 37},
+    {id: '2', name: 'Edu', age: 37},
+    {id: '3', name: 'Julia', age: 18},
+    {id: '4', name: 'Gabi', age: 18}
+  ]
+
   return (
     <SafeAreaView>
       {/* <StatusBar/> */}
 
-      <Person name='Carlos' age={37}/>
-      <Person name='Edu' age={37}/>
-      <Person name='Julia' age={18}/>
-      <Person name='Gabi' age={18}/>
+      {/* Usando flatlist */}
+      <FlatList
+        data={peopleList}
+        keyExtractor={(item) => item.id}
+
+        renderItem={({item}) => (
+          //Exibir cada item da lista
+          <Person name={item.name} age={item.age}/>
+        )}
+      />
     </SafeAreaView>
   );
 }
