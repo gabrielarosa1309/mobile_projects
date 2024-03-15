54f6583fe9a8841ead11b2bfe90910395ac8730d
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-03-15 07:57:45 -0300

    :sparkles: mc donalds se deus quiser

--- cameraExample/App.js
+++ cameraExample/App.js
@@ -1,31 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Image, Alert } from 'react-native';
 
+//1- instanciar a camera
 import { Camera, CameraType } from 'expo-camera';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { FontAwesome } from "@expo/vector-icons"
+import * as MediaLibrary from "expo-media-library"
 
 export default function App() {
-  const [tipoCamera, setTipoCamera] = useState(Camera.Constants.Type.back)
+
+  const cameraRef = useRef(null);
+
+  const [photo, setPhoto] = useState(null)
+
+  const [openModal, setOpenModal] = useState(false)
+
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
+
+  async function CapturePhoto() {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setPhoto(photo.uri)
+
+      setOpenModal(true)
+
+      console.log(photo);
+    }
+  }
+
+  async function ClearPhoto() {
+    setPhoto(null)
+    setOpenModal(false)
+  }
+
+  async function SavePhoto() {
+    if(photo) {
+      await MediaLibrary.createAssetAsync(photo)
+      .then( () => {
+        Alert.alert("Sucesso", "Foto salva na galeria")
+      } ).catch(error => {
+        Alert.alert("Erro", "Erro ao salvar foto")
+      })
+
+      setOpenModal(false)
+    }
+  }
 
   useEffect(() => {
     (async () => {
       const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync()
+
+      const {status: mediaStatus} = await MediaLibrary.requestPermissionsAsync()
     })();
   }, [])
 
+
   return (
-    <View style={ styles.container }>
+    <View style={styles.container}>
+      {/* 2- chamando a camera */}
       <Camera
-        type={tipoCamera}
-        style={ styles.camera }
+        ref={cameraRef}
+        type={cameraType}
+        style={styles.camera}
+
         ratio={'16:9'}
       >
-        <View style={ styles.viewFlip }>
-          <TouchableOpacity style={ styles.btnFlip } onPress={() => setTipoCamera(tipoCamera == CameraType.front ? CameraType.back : CameraType.front)}>
-            <Text style={ styles.txtFlip }>Trocar</Text>
+
+        <View style={styles.viewFlip}>
+          <TouchableOpacity style={styles.btnFlip} onPress={() => setCameraType(cameraType == CameraType.front ? CameraType.back : CameraType.front)}>
+            <Text style={styles.txtFlip}>Trocar</Text>
           </TouchableOpacity>
         </View>
+
       </Camera>
+
+      <TouchableOpacity style={styles.btnCapture} onPress={() => CapturePhoto()}>
+        <FontAwesome
+          name='camera'
+          size={23}
+          color={"#fff"}
+        />
+      </TouchableOpacity>
+
+      <Modal animationType='slide' transparent={false} visible={openModal}>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center", padding: 30 }}>
+          <Image style={{ width: '100%', height: 500, borderRadius: 10 }} source={{ uri: photo }} />
+          <View style={{ margin: 15, flexDirection: 'row' }}>
+            <TouchableOpacity style={styles.btnCancel} onPress={() => ClearPhoto()}>
+              <FontAwesome
+                name='trash'
+                size={40}
+                color={"#ff0000"}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btnSave} onPress={() => SavePhoto()}>
+              <FontAwesome
+                name='save'
+                size={40}
+                color={"#121212"}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -37,28 +114,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
   camera: {
     flex: 1,
     width: '100%',
-    height: '80%',
+    height: '80%'
   },
-
   viewFlip: {
     flex: 1,
     backgroundColor: 'transparent',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center"
   },
-
   btnFlip: {
-    bottom: 20,
     padding: 15
   },
-
   txtFlip: {
     fontSize: 20,
-    color: '#ffffff'
+    color: "#fff",
+    marginBottom: 20
+  },
+  btnCapture: {
+    margin: 20,
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: "#121212",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnCancel:{
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: "transparent",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnSave:{
+    padding: 20,
+    backgroundColor: "transparent",
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
